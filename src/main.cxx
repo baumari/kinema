caabe61955964120a8667abec85e4f7582bf7551
baumari <kento@nh.scphys.kyoto-u.ac.jp>
2019-03-27 14:11:30 +0900

    getopt added
    option -g is added

--- src/main.cxx
+++ src/main.cxx
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <unistd.h>
+
+void usage();
 
 int main(int argc, char **argv){
-  return 0;
+  int opt;
+  int f_graph = 0;
+  while((opt = getopt(argc, argv, "g")) != -1){
+    switch(opt){
+    case 'g':
+      f_graph = 1;
+      break;
+    default:
+      usage();
+      return -1;
+    }
+  }
+}
+
+void usage(){
+  printf("Usage: ./kinema [-g]\n");
+  printf("-g: Graph will be displayed.\n");
+  return;
 }
