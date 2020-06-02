dda2b1b58244fe433f49643ab8a3847217fdb32b
baumari <kento@nh.scphys.kyoto-u.ac.jp>
2020-06-02 13:02:38 +0900

    KError modified

--- include/KError.hh
+++ include/KError.hh
@@ -1,15 +1,16 @@
 #ifndef K_ERROR_HH
 #define K_ERROR_HH
 
-typedef struct {
-  enum Error_Code
-    { 
-     PARTICLE_DATA_NOT_FOUND=1,
-     ZERO_DIVISION,
-     INVALID_ARGUMENT,
-     NULLPTR_FOUND,
-     
-     ERR_CODE_SIZE,
-    };
-} KError;
+namespace KError{
+enum Error_Code
+  {
+   SUCCESS = 0,
+   PARTICLE_DATA_NOT_FOUND,
+   ZERO_DIVISION,
+   INVALID_ARGUMENT,
+   NULLPTR_FOUND,
+   
+   ERR_CODE_SIZE,
+  };
+}
 #endif
